b1d3a4386afb7985ba3c375ef325d0a4c8cb53d5
Matthew Yee <yee.matthew@gmail.com>
2026-08-20 11:12:50 -0700

    Add Satellite teardown steps to setup-satellite.sh
    - Unregister Satellite server from itself
    - Delete Satellite host from inventory
    - Delete the Satellite Server activation key

--- setup-automation/setup-satellite.sh
+++ setup-automation/setup-satellite.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# Unregister Satellite server from itself.
+subscription-manager unregister
+
+# Delete Satellite from its inventory.
+hammer host delete --name satellite.lab
+
+# Delete the Activation Key created for the Satellite server.
+hammer activation-key delete --name "Satellite Server" --organization "Acme Org"
