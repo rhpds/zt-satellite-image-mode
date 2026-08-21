d727594cfddc371c8e9e57b5fcf5dea3e69ba73c
Matthew Yee <yee.matthew@gmail.com>
2026-08-21 10:54:57 -0700

    Add registry authentication to rhel1 setup script
    - Configure registry.redhat.io pull credentials using REGISTRY_PULL_TOKEN
    - Run podman login with generated auth.json
    - Remove auth.json after login to clean up credentials

--- setup-automation/setup-rhel1.sh
+++ setup-automation/setup-rhel1.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+mkdir -p ~/.config/containers
+cat <<EOF> ~/.config/containers/auth.json
+{
+    "auths": {
+      "registry.redhat.io": {
+        "auth": "${REGISTRY_PULL_TOKEN}"
+      }
+    }
+  }
+EOF
+
+podman login registry.redhat.io --authfile ~/.config/containers/auth.json
+
+rm ~/.config/containers/auth.json
